921f1cbe3076ea4dbc5eb97eed6351e053f91d82
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-06-29 05:45:53 +0200

    Add basic app layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <div className="navbar">
+
+      </div>
+      <div className="main">
+
+      </div>
+      <div className="footer">
+        
+      </div>
     </div>
   );
 }
