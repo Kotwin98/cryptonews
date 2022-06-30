42867de444f8556171bb15f6954dc3372f81a35b
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-06-30 07:57:55 +0200

    Import navbar component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar } from './components';
 
 import './App.css';
 
@@ -6,13 +8,13 @@ function App() {
   return (
     <div className="app">
       <div className="navbar">
-
+        <Navbar />
       </div>
       <div className="main">
 
       </div>
       <div className="footer">
-        
+
       </div>
     </div>
   );
