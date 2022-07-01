fa944db34b9f377ea268eb9660b0683f0aa78127
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-07-01 06:27:41 +0200

    Add dummy components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar } from './components';
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from './components';
 
 import './App.css';
 
@@ -11,7 +11,22 @@ function App() {
         <Navbar />
       </div>
       <div className="main">
-
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              
+              <Route path="/exchanges" element={<Exchanges />} />
+                
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+                
+              <Route path="/crypto/:coinId" element={<CryptoDetails />} />
+               
+              <Route path="/news" element={<News />} />
+               
+            </Routes>
+          </div>
+        </Layout>
       </div>
       <div className="footer">
 
