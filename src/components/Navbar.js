00b3b3a4df9b1e6f4c9ab70f4edbd2fbfe234a49
Mateusz Kotwiński <mkotwinski98@gmail.com>
2022-07-01 06:29:44 +0200

    Add url paths

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,9 +11,20 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/">Cryptonews</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container">
-
-        </Button> */}
+        <Menu>
+          <Menu.Item icon={<HomeOutlined />}>
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Item icon={<FundOutlined />}>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+          </Menu.Item>
+          <Menu.Item icon={<MoneyCollectOutlined />}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Item icon={<BulbOutlined />}>
+            <Link to="/news">News</Link>
+          </Menu.Item>
+        </Menu>
       </div>
     </div>
   )
